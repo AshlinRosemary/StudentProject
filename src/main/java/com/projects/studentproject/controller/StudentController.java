419b0e81de18beb7aa9db2767db843649e3c375c
AshlinRosemary <ashlinrosemary@gmail.com>
2023-08-08 15:11:45 +0530

    Added List RestAPI

--- src/main/java/com/projects/studentproject/controller/StudentController.java
+++ src/main/java/com/projects/studentproject/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.projects.studentproject.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 //Here passing Student class as datatype , getStudent method to get our JSOn
@@ -11,6 +14,17 @@ public class StudentController {
     public Student getStudent(){
         Student student=new Student(1,"Ashli",10);
         return student;
+    }
+    // http://localhost:8080/students
+    @GetMapping("students")
+    public List<Student> getStudents(){
+
+        List<Student> students=new ArrayList<>();
+        students.add(new Student(1,"Mariam",4));
+        students.add(new Student(2,"Asla",5));
+        students.add(new Student(3,"Rosy",7));
+        students.add(new Student(4,"Mary",8));
+        return students;
     }
 
 }
